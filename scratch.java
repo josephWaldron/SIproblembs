167af573cbbcd61d04d0b8ca2d692539654d62be
JosephWaldron <waldrojo@kean.edu>
2022-01-28 09:09:33 -0500

    1

---
+++ scratch.java
@@ -0,0 +1,10 @@
+public class scratch {
+    public static void main(String[] args) {
+        int i =5;
+        for(int j = 1; j < 7; j++){
+            i = i-2;
+            System.out.println(i);
+            System.out.println(j);
+        }
+    }
+}
